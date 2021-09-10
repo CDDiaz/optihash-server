eb56c200c24d5e06903d01849d59866b5a8b1438
CDDiaz <cabj_xeneize@hotmail.com>
2021-09-10 11:17:01 +1000

    backend to heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000' # deployed front-end client’s domain address XZE
+    origins 'http://localhost:3000', 'http://localhost:3001', 'https://optihash-server.herokuapp.com' # deployed front-end client’s domain address XZE
 
     resource '*',
       headers: :any,
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_optihash', domain: 'your-frontend-domain' # XZE
+  Rails.application.config.session_store :cookie_store, key: '_optihash', domain: 'https://optihash.herokuapp.com' # XZE
 else
   Rails.application.config.session_store :cookie_store, key: '_optihash'
 end
